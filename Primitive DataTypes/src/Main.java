2795c8dc6a02c4c15b7e8dd36f5c35338204bcdb
ABZ98990 <master.aggressive@gmail.com>
2024-03-28 15:37:35 +0500

    Primitive Data Types

---
+++ Primitive DataTypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //primitive Data Types
+        byte age = 30;
+        int v_count = 123_456_789;
+        long count = 3_123_456_789L;
+        double d = 10.99;
+        float f = 10.99F;
+        char letter = 'A';
+        boolean isTrue = false;
+        System.out.println(count);
+    }
+}
