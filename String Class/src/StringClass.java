0e1dba99c6c0f8f720887520a7fa494ab5084334
ABZ98990 <master.aggressive@gmail.com>
2024-03-28 16:16:48 +0500

    String class operations

---
+++ String Class/src/StringClass.java
@@ -0,0 +1,13 @@
+public class StringClass
+{
+    public static void main(String[] args)
+    {
+        //string class variable declaration
+        String message = new String("Hello World");
+        System.out.println(message);
+
+        //short declaration of string class variables
+        String message2 = "Hello World Two";
+        System.out.println(message2);
+    }
+}
