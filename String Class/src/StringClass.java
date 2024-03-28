a62d6c19561dda241168f2d661aba98328c4be9b
ABZ98990 <master.aggressive@gmail.com>
2024-03-28 18:10:19 +0500

    functions and escape sequences
    Utilizing string class methods and escape sequences to address string manipulation operations.

--- String Class/src/StringClass.java
+++ String Class/src/StringClass.java
@@ -2,12 +2,28 @@ public class StringClass
 {
     public static void main(String[] args)
     {
-        //string class variable declaration
+        /* string class variable declaration
+
         String message = new String("Hello World");
         System.out.println(message);
+        */
 
-        //short declaration of string class variables
-        String message2 = "Hello World Two";
-        System.out.println(message2);
+
+        /* short declaration of string class variables
+
+        String message = "Hello World" + "!!";
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("W"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("l"));
+        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("l", "w"));
+        System.out.println(message.trim()); //trim unnecessary blank spaces in strings
+        */
+
+        //escape sequences in strings
+        String message = "D:\\!repos\\Java-Development\\Java-Dev";
+        System.out.println(message);
+
     }
 }
