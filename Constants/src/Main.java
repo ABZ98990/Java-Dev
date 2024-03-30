412f9da5f34ed69822922d5b71f9ad72dd4cb792
ABZ98990 <master.aggressive@gmail.com>
2024-03-30 23:59:01 +0500

    Constant variables
    Constant variable declaration and conventional approach to address them.

---
+++ Constants/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Constant declaration of variables
+        final float pi = 3.14F;
+        System.out.println(pi);
+        //Conventional methods
+        final float PI = 3.14F; //always use uppercase letters for naming constant variables
+        System.out.println(PI);
+    }
+}
