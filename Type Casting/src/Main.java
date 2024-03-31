755d96ed42f5c4deedbfee49471fbef07e04e612
ABZ98990 <master.aggressive@gmail.com>
2024-03-31 15:21:25 +0500

    all types of casting
    explained implicit and explicit casting. Used wrapper class to cast strings into other variable types.

--- Type Casting/src/Main.java
+++ Type Casting/src/Main.java
@@ -5,6 +5,25 @@ public class Main
         //implicit casting (automatic)
         short x = 1;
         int y = x + 2;
-        System.out.println(y);
+        System.out.println("y : " + y);
+
+        //explicit casting
+        double a = 1.1;
+        int b = (int) (a + 2);
+        System.out.println("b : " + b);
+
+        double f = 1.1;
+        double g = (double) f + (double) 2;
+        System.out.println("g : " + g);
+
+        //string casting using wrapper class
+        String str = "1";
+        int r = Integer.parseInt(str) + 2;
+        System.out.println("r : " + r);
+
+        //string being casted into float using wrapper class
+        String s = "1.1";
+        float u = Float.parseFloat(s) + 2;
+        System.out.println("u : " + u);
     }
 }
