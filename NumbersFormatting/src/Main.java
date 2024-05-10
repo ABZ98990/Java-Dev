d0104403ca9f4f8b8b1c937ef36201d66cfece89
ABZ98990 <master.aggressive@gmail.com>
2024-05-10 20:28:09 +0500

    added getPercentageInstance();

--- NumbersFormatting/src/Main.java
+++ NumbersFormatting/src/Main.java
@@ -7,9 +7,14 @@ public class Main
         //cannot make new instance with 'new' keyword because the class is Abstract
         //NumberFormat currency = new NumberFormat();
 
-        //using 'Factory Method'
+        //currency formatting with the use of a 'Factory Method'
         NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(1234567.89);
-        System.out.println(result);
+        String currency_result = currency.format(1234567.89);
+        System.out.println(currency_result);
+
+        //percentage formatting with the use of a 'Factory Method'
+        NumberFormat percentage = NumberFormat.getPercentInstance();
+        String percentage_result = percentage.format(0.45);
+        System.out.println(percentage_result);
     }
 }
