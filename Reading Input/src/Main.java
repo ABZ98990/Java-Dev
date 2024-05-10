e02a5af17a0356f23d5d78826a47fb7a389d5273
ABZ98990 <master.aggressive@gmail.com>
2024-05-10 20:46:35 +0500

    nextLine() vs next() in String input

--- Reading Input/src/Main.java
+++ Reading Input/src/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Reading integer input using Scanner Class
         Scanner sc = new Scanner(System.in);
+        /*//Reading integer input using Scanner Class
         System.out.print("Enter a number: ");
         System.out.println(sc.nextInt());
 
@@ -18,5 +18,14 @@ public class Main
 
         System.out.print("Enter a byte value: ");
         System.out.println(sc.nextByte());
+        */
+
+        //Reading Strings using nextLine()
+        System.out.print("Enter a string value: ");
+        System.out.println(sc.nextLine());
+
+        //Reading Strings using next()
+        System.out.print("Enter a multi-word string line: ");
+        System.out.println(sc.next()); //next() only reads the first token of the input text
     }
 }
