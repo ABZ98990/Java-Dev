5b5d10fdfaf326070469ec8b41f8fa54bc60cc31
ABZ98990 <master.aggressive@gmail.com>
2024-05-13 16:31:42 +0500

    added support for recursive guesses

--- Practice Exercise- FizzBuzz/src/Main.java
+++ Practice Exercise- FizzBuzz/src/Main.java
@@ -1,26 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = sc.nextInt();
-        if (number%5 == 0)
+        while (true)
         {
-            System.out.println("Fizz");
-        }
-        else if (number%3 == 0)
-        {
-            System.out.println("Buzz");
-        }
-        else if ((number % 5 == 0) && (number % 3 == 0))
-        {
-            System.out.println("FizzBuzz");
-        } else
-        {
-            System.out.println(number);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            if (((number%5 == 0)  && (number % 3 == 0)))
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (number%3 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else if (number % 5 == 0)
+            {
+                System.out.println("Fizz");
+            } else
+            {
+                System.out.println(number);
+            }
+            System.out.print("Do you wish to try again? [y/n]: ");
+            String con = sc.next();
+            if (Objects.equals(con,"n"))
+            {
+                break;
+            }
         }
     }
 }
